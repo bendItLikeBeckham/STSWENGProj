62c3da2631ecf789ac0644c7f4feb6b27748449e
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-07-11 22:33:34 +0800

    API Test
    Created API Testing for the storing of data of time in and time out on the database and validate if the values stored in the database are correct.

---
+++ cypress/e2e/1-getting-started/API-employee-clockpage.cy.js
@@ -0,0 +1,80 @@
+/// <reference types="cypress" />
+
+
+describe('Time-in API Test', () => {
+
+    const weekdayIndex = new Date().getDay();
+
+
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+        cy.get("#email").type("john_doe@gmail.com")
+        cy.get("#password").type("123", { log: false })
+        cy.get("#login-button").click()
+      })
+
+      it("should store the time-in value in the database", () => {
+
+        cy.get("#time-in-btn").click()
+
+        const current_time = new Date();
+        let hours = current_time.getHours();
+        let minutes = current_time.getMinutes();
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        const formattedTime = hours + ':' + minutes;
+
+        if(weekdayIndex == 1) {
+            cy.findOne({Mon_Time_In:formattedTime});
+        } else if (weekdayIndex == 2) {
+            cy.findOne({Tue_Time_In:formattedTime});
+        } else if (weekdayIndex == 3) {
+            cy.findOne({Wed_Time_In:formattedTime});
+        } else if (weekdayIndex == 4) {
+            cy.findOne({Thu_Time_In:formattedTime});
+        } else if (weekdayIndex == 5) {
+            cy.findOne({Fri_Time_In:formattedTime});
+        } else if (weekdayIndex == 6) {
+            cy.findOne({Sat_Time_In:formattedTime});
+        } else if (weekdayIndex == 0) {
+            cy.findOne({Sun_Time_In:formattedTime});
+        }
+
+        cy.get("#popup-3 .close-btn").click()
+      })
+
+
+      it("should store the time-out value in the database", () => {
+
+        cy.get("#time-out-btn").click()
+
+        const current_time = new Date();
+        let hours = current_time.getHours();
+        let minutes = current_time.getMinutes();
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        const formattedTime = hours + ':' + minutes;
+
+        if(weekdayIndex == 1) {
+            cy.findOne({Mon_Time_Out:formattedTime});
+        } else if (weekdayIndex == 2) {
+            cy.findOne({Tue_Time_Out:formattedTime});
+        } else if (weekdayIndex == 3) {
+            cy.findOne({Wed_Time_Out:formattedTime});
+        } else if (weekdayIndex == 4) {
+            cy.findOne({Thu_Time_Out:formattedTime});
+        } else if (weekdayIndex == 5) {
+            cy.findOne({Fri_Time_Out:formattedTime});
+        } else if (weekdayIndex == 6) {
+            cy.findOne({Sat_Time_Out:formattedTime});
+        } else if (weekdayIndex == 0) {
+            cy.findOne({Sun_Time_Out:formattedTime});
+        }
+
+        cy.get("#popup-4 .close-btn").click()
+      })
+
+  });
+  
