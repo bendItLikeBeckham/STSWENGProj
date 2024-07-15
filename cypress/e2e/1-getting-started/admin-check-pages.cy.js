35fa9d53c6f95deeed4765380436a811c91b5811
Gabriele De Guzman <gabr.degu@gmail.com>
2024-07-15 18:06:29 +0800

    added admin cypress tests

---
+++ cypress/e2e/1-getting-started/admin-check-pages.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('Application Exploration Admin Test', () =>{
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+        cy.get("#email").type("adminusertest111@gmail.com")
+        cy.get("#password").type("111", { log: false })
+        cy.get("#login-button").click()
+      })
+
+      it('should log-in to correct page', () =>{
+        cy.url().should("include", "/admin_dashboard")
+      })
+
+      it('should go to employee management', () =>{
+        cy.get('#emp-mgm-id').click()
+        cy.url().should("include", "admin_empman_attendrecs")
+      })
+
+      it('should go to employee management', () =>{
+        cy.get('#adm-dlt').click
+        cy.get('#adm-dlt')
+      })
+
+      it('should go to notifications', () =>{
+        cy.get('#adm-not').click
+        cy.get('.admin-leftdash-top > h2')
+      })
+
+    
+})  
